455732c1593a04f18b997e6d110f379dc1117e73
Franzl76 <60292525+Franzl76@users.noreply.github.com>
2021-12-15 07:30:25 +0000

    Add files via upload

---
+++ 15. Reference types - Objects & Arrays/Arrays.js
@@ -0,0 +1,15 @@
+let selectedColors = ['green', 'blue', 'red'];
+console.log(selectedColors);
+//toon de lengte van de array
+console.log(selectedColors.length);
+//toon de eerste positie van de array
+console.log(selectedColors[0]);
+//toon de laatste positie van de array
+console.log(selectedColors[2]);
+//een vierde element toevoegen aan de array
+selectedColors[3] = 'Yellow';
+//een getal toevoegen aan de array
+selectedColors[4] = 51;
+//tekst toevoegen aan de array
+selectedColors[5] = {greeting: 'hi, I am an object'};
+console.log(selectedColors[4]);
---
+++ 15. Reference types - Objects & Arrays/Objects.js
@@ -0,0 +1,17 @@
+let person = {
+    name: 'Frans',
+    age: 30
+};
+
+console.log(person);
+
+//Dot Notation
+person.name = 'Frans';
+person.age = 62;
+console.log(person.name);
+console.log(person.age);
+
+//Bracket Notation
+person['name'] = 'Frans';
+person['age'] = 62;
+console.log(person);
